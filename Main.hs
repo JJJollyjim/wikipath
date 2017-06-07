bfaad7e87acfac98804542d38a108900bc263cb2
Jamie McClymont <jamie.mcclymont@gmail.com>
2017-06-08 00:52:57 +1200

    Make fasterer

--- Main.hs
+++ Main.hs
@@ -18,34 +18,50 @@ insertLines = filter (B.isPrefixOf "INSERT INTO `page` VALUES") <$> B.lines <$> 
 
 row :: Parser Row
 row = do
-  _ <- P.char '('
+  -- As an optimisation, the varbinaries used to store very simple non-user-entered info are skipped, instead of using the expensive quotedString parser
+  P.char '('
   id <- P.decimal
-  _ <- P.char ','
+  P.char ','
   namespace <- P.decimal
-  _ <- P.char ','
+  P.char ','
   title <- quotedString
   _ <- P.char ','
-  _ <- quotedString
+  -- Restrictions
+  P.skipWhile (\c -> c /= ',')
   _ <- P.char ','
-  _ <- P.decimal
+  _ <- P.skipWhile (\c -> c /= ',')
   _ <- P.char ','
-  _ <- P.decimal
+  _ <- P.skipWhile (\c -> c /= ',')
   _ <- P.char ','
-  _ <- P.decimal
+  _ <- P.skipWhile (\c -> c /= ',')
   _ <- P.char ','
-  _ <- P.double
+  _ <- P.skipWhile (\c -> c /= ',')
   _ <- P.char ','
-  _ <- P.string "NULL" <|> quotedString
+  -- Timestamp https://www.mediawiki.org/wiki/Manual:Timestamp
+  P.char '\''
+  P.skipWhile (\c -> c /= ',' && c /= '\'')
+  P.char '\''
   _ <- P.char ','
-  _ <- P.string "NULL" <|> quotedString
+  -- Timestamp https://www.mediawiki.org/wiki/Manual:Timestamp
+  P.skipWhile (\c -> c /= ',' )
   _ <- P.char ','
-  _ <- P.decimal
+  _ <- P.skipWhile (\c -> c /= ',')
   _ <- P.char ','
-  _ <- P.decimal
+  _ <- P.skipWhile (\c -> c /= ',')
   _ <- P.char ','
-  _ <- quotedString
+  -- Content model https://www.mediawiki.org/wiki/Content_handlers
+  -- Wiki claims it's always [a-zA-Z\-]
+  -- Quotes are here as a sanity check, hopefully unnecessary...
+  -- P.skipWhile (\c -> c /= ',' && c /= '\'')
+  P.char '\''
+  P.skipWhile (\c -> c /= ',' && c /= '\'')
+  P.char '\''
   _ <- P.char ','
-  _ <- P.string "NULL" <|> quotedString
+  -- Lang - I'm 70% sure this won't contain escapes or anything...
+  -- Usually NULL, I haven't actually spotted any string versions in the wild
+  --_ <- quotedString
+  _ <- P.string "NULL" <|> P.takeWhile (\c -> c /= ',' && c /= ')')
+  -- P.skipWhile (\c -> c /= ',' && c /= ')' && c /= '\'')
   _ <- P.char ')'
   return $ Row id namespace title
 
@@ -56,11 +72,11 @@ escape = do
 
 {- https://stackoverflow.com/questions/24106314/parser-for-quoted-string-using-parsec -}
 quotedString :: Parser BS.ByteString
-quotedString = do
+quotedString = (do
   char '\''
-  chars <- BS.concat <$> many' (P.takeWhile1 (\w8 -> w8 /= '\\' && w8 /= '\'') <|> escape)
+  chars <- BS.concat <$> (many' (P.takeWhile1 (\w8 -> w8 /= '\\' && w8 /= '\'') <|> escape))
   char '\''
-  return $ chars
+  return $ chars) <|> "''"
 
 isRight :: Either a b -> b
 isRight (Right x) = x
